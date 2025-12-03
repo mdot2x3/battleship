3ff240965879777934b9760e2b1fe145d8b35dce
mdot2x3 <mdot2x3@gmail.com>
2025-12-02 16:32:25 -0800

    feat: continue Ship class construction
    write additional TDD tests in ship.test.js and
    update the Ship class in ship.js to make the tests
    pass.
    
    add a validation check to the Ship class
    constructor to ensure the length argument entered
    is a number within the assigned range.
    
    add new properties hitCount and isSunkStatus and
    new methods hit() that keeps track of the number
    of hits the ship has taken and isSunk() that
    checks if the ship is sunk by comparing hitCount
    and the length of the ship.

--- src/ship.js
+++ src/ship.js
@@ -1,5 +1,22 @@
 export class Ship {
   constructor(length) {
+    if (typeof length !== "number" || length < 2 || length > 5) {
+      throw new Error(
+        "Ship length must be no less than 2 and no greater than 5.",
+      );
+    }
     this.length = length;
+    this.hitCount = 0;
+    this.isSunkStatus = false;
+  }
+
+  hit() {
+    this.hitCount += 1;
+    return this.hitCount;
+  }
+
+  isSunk() {
+    if (this.hitCount >= this.length) this.isSunkStatus = true;
+    return this.isSunkStatus;
   }
 }
--- src/ship.test.js
+++ src/ship.test.js
@@ -1,8 +1,43 @@
 import { Ship } from "./ship.js";
 
 describe("Ship", () => {
+  let ship;
+
+  beforeEach(() => {
+    ship = new Ship(4);
+  });
+
   test("Ship has a length property", () => {
-    const ship = new Ship(900);
-    expect(ship.length).toBe(900);
+    expect(ship.length).toBe(4);
+  });
+
+  test("Ship throws an error if length is not a valid number", () => {
+    expect(() => new Ship("abc")).toThrow();
+    expect(() => new Ship(12)).toThrow();
+    expect(() => new Ship()).toThrow();
+  });
+
+  test("Ship has a hitCount property that starts at 0", () => {
+    expect(ship.hitCount).toBe(0);
+  });
+
+  test("Ship has a hit method that increases hitCount", () => {
+    expect(ship.hit()).toBe(1);
+    expect(ship.hit()).toBe(2);
+    expect(ship.hit()).toBe(3);
+  });
+
+  test("Ship has a Boolean isSunk() property", () => {
+    expect(ship.isSunk()).toBe(false);
+  });
+
+  test("isSunk method returns true when hitCount equals length", () => {
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    ship.hit();
+    expect(ship.isSunk()).toBe(true);
+    ship.hitCount = 100;
+    expect(ship.isSunk()).toBe(true);
   });
 });
