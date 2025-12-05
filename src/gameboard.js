6f1ece2c3d411b2b688800c7e489f107c205ac5a
mdot2x3 <mdot2x3@gmail.com>
2025-12-05 13:34:04 -0800

    refactor: modify Gameboard logic and tests
    add new test case for the receiveAttack method in
    gameboard.test.js that takes coordinates to check
    for ship hits.
    
    build simple receiveAttack method in gameboard.js
    that accepts coordinates and returns a message to
    make the test pass as an initial step.
    
    refactor Gameboard class properties and the
    placeShip method in gameboard.js to refine the
    logic and method of ship placement on the
    gameboard, specifically, have ships cover the
    correct number of coordinates based on their
    starting coordinates, orientation, and length.

--- src/gameboard.js
+++ src/gameboard.js
@@ -4,11 +4,28 @@ export class Gameboard {
   constructor() {
     this.length = 10;
     this.height = 10;
-    this.shipOne = [0, 0, ""];
-    this.shipTwo = [0, 0, ""];
-    this.shipThree = [0, 0, ""];
-    this.shipFour = [0, 0, ""];
-    this.shipFive = [0, 0, ""];
+    this.shipOneOrientation = "";
+    this.shipTwoOrientation = "";
+    this.shipThreeOrientation = "";
+    this.shipFourOrientation = "";
+    this.shipFiveOrientation = "";
+    this.shipOne = [[0, 0]];
+    this.shipTwo = [[0, 0]];
+    this.shipThree = [[0, 0]];
+    this.shipFour = [[0, 0]];
+    this.shipFive = [[0, 0]];
+  }
+
+  // helper function to ensure input coordinates are valid
+  inBounds(xCoord, yCoord) {
+    if (xCoord < 0 || xCoord > 10)
+      throw new Error(
+        "X Coordinate must be no less than 0 and no greater than 10.",
+      );
+    if (yCoord < 0 || yCoord > 10)
+      throw new Error(
+        "Y Coordinate must be no less than 0 and no greater than 10.",
+      );
   }
 
   placeShips(ship, xCoord, yCoord, orientation) {
@@ -20,40 +37,84 @@ export class Gameboard {
     if (formatOrientation !== "h" && formatOrientation !== "v")
       throw new Error("Invalid orientation entry.");
 
-    if (xCoord < 0 || xCoord > 10)
-      throw new Error(
-        "X Coordinate must be no less than 0 and no greater than 10.",
-      );
-    if (yCoord < 0 || yCoord > 10)
-      throw new Error(
-        "Y Coordinate must be no less than 0 and no greater than 10.",
-      );
+    this.inBounds(xCoord, yCoord);
 
     switch (formatShip) {
       case "carrier":
         newShip = new Ship(5);
         shipProperty = "shipOne";
-        this.shipOne = [xCoord, yCoord, formatOrientation];
+        shipOrientation = "shipOneOrientation";
+        this.shipOne = [[xCoord, yCoord]];
+        this.shipOneOrientation = formatOrientation;
+        if (this.shipOneOrientation === "h") {
+          for (let i = 1; i < 5; i++) {
+            this.shipOne.push([xCoord + i, yCoord]);
+          }
+        } else if (this.shipOneOrientation === "v") {
+          for (let i = 1; i < 5; i++) {
+            this.shipOne.push([xCoord, yCoord + i]);
+          }
+        }
         break;
       case "battleship":
         newShip = new Ship(4);
         shipProperty = "shipTwo";
-        this.shipTwo = [xCoord, yCoord, formatOrientation];
+        shipOrientation = "shipTwoOrientation";
+        this.shipTwo = [[xCoord, yCoord]];
+        this.shipTwoOrientation = formatOrientation;
+        if (this.shipTwoOrientation === "h") {
+          for (let i = 1; i < 4; i++) {
+            this.shipTwo.push([xCoord + i, yCoord]);
+          }
+        } else if (this.shipTwoOrientation === "v") {
+          for (let i = 1; i < 4; i++) {
+            this.shipTwo.push([xCoord, yCoord + i]);
+          }
+        }
         break;
       case "cruiser":
         newShip = new Ship(3);
         shipProperty = "shipThree";
-        this.shipThree = [xCoord, yCoord, formatOrientation];
+        shipOrientation = "shipThreeOrientation";
+        this.shipThree = [[xCoord, yCoord]];
+        this.shipThreeOrientation = formatOrientation;
+        if (this.shipThreeOrientation === "h") {
+          for (let i = 1; i < 3; i++) {
+            this.shipThree.push([xCoord + i, yCoord]);
+          }
+        } else if (this.shipThreeOrientation === "v") {
+          for (let i = 1; i < 3; i++) {
+            this.shipThree.push([xCoord, yCoord + i]);
+          }
+        }
         break;
       case "submarine":
         newShip = new Ship(3);
         shipProperty = "shipFour";
-        this.shipFour = [xCoord, yCoord, formatOrientation];
+        shipOrientation = "shipFourOrientation";
+        this.shipFour = [[xCoord, yCoord]];
+        this.shipFourOrientation = formatOrientation;
+        if (this.shipFourOrientation === "h") {
+          for (let i = 1; i < 3; i++) {
+            this.shipFour.push([xCoord + i, yCoord]);
+          }
+        } else if (this.shipFourOrientation === "v") {
+          for (let i = 1; i < 3; i++) {
+            this.shipFour.push([xCoord, yCoord + i]);
+          }
+        }
         break;
       case "destroyer":
         newShip = new Ship(2);
         shipProperty = "shipFive";
-        this.shipFive = [xCoord, yCoord, formatOrientation];
+        shipOrientation = "shipFiveOrientation";
+        this.shipFive = [[xCoord, yCoord]];
+        this.shipFiveOrientation = formatOrientation;
+        if (this.shipFiveOrientation === "h") {
+          this.shipFive.push([xCoord + 1, yCoord]);
+        } else if (this.shipFiveOrientation === "v") {
+          this.shipFive.push([xCoord, yCoord + 1]);
+        }
         break;
       default:
         throw new Error("Invalid ship entry.");
@@ -61,8 +122,13 @@ export class Gameboard {
 
     return {
       shipLength: newShip.length,
-      shipCoordinates: [this[shipProperty][0], this[shipProperty][1]],
-      shipOrientation: this[shipProperty][2],
+      shipCoordinates: this[shipProperty],
+      shipOrientation: this[shipOrientation],
     };
+  }
+
+  receiveAttack(xCoord, yCoord) {
+    this.inBounds(xCoord, yCoord);
+    return "hit";
   }
 }
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -10,7 +10,13 @@ describe("Gameboard", () => {
   test("Gameboard has a placeShips method", () => {
     expect(gameboard.placeShips("carrier", 5, 5, "v")).toStrictEqual({
       shipLength: 5,
-      shipCoordinates: [5, 5],
+      shipCoordinates: [
+        [5, 5],
+        [5, 6],
+        [5, 7],
+        [5, 8],
+        [5, 9],
+      ],
       shipOrientation: "v",
     });
   });
@@ -18,37 +24,68 @@ describe("Gameboard", () => {
   test("placeShips method should accept valid types of ships", () => {
     expect(gameboard.placeShips("Carrier", 2, 4, "h")).toStrictEqual({
       shipLength: 5,
-      shipCoordinates: [2, 4],
+      shipCoordinates: [
+        [2, 4],
+        [3, 4],
+        [4, 4],
+        [5, 4],
+        [6, 4],
+      ],
       shipOrientation: "h",
     });
     expect(gameboard.placeShips("Battleship", 1, 3, "v")).toStrictEqual({
       shipLength: 4,
-      shipCoordinates: [1, 3],
+      shipCoordinates: [
+        [1, 3],
+        [1, 4],
+        [1, 5],
+        [1, 6],
+      ],
       shipOrientation: "v",
     });
     expect(gameboard.placeShips("Cruiser", 8, 9, "V")).toStrictEqual({
       shipLength: 3,
-      shipCoordinates: [8, 9],
+      shipCoordinates: [
+        [8, 9],
+        [8, 10],
+        [8, 11],
+      ],
       shipOrientation: "v",
     });
     expect(gameboard.placeShips("Submarine", 0, 1, "H")).toStrictEqual({
       shipLength: 3,
-      shipCoordinates: [0, 1],
+      shipCoordinates: [
+        [0, 1],
+        [1, 1],
+        [2, 1],
+      ],
       shipOrientation: "h",
     });
     expect(gameboard.placeShips("Destroyer", 10, 10, "h")).toStrictEqual({
       shipLength: 2,
-      shipCoordinates: [10, 10],
+      shipCoordinates: [
+        [10, 10],
+        [11, 10],
+      ],
       shipOrientation: "h",
     });
     expect(gameboard.placeShips("BATTLESHIP", 7, 5, "h")).toStrictEqual({
       shipLength: 4,
-      shipCoordinates: [7, 5],
+      shipCoordinates: [
+        [7, 5],
+        [8, 5],
+        [9, 5],
+        [10, 5],
+      ],
       shipOrientation: "h",
     });
     expect(gameboard.placeShips("submarine", 9, 8, "H")).toStrictEqual({
       shipLength: 3,
-      shipCoordinates: [9, 8],
+      shipCoordinates: [
+        [9, 8],
+        [10, 8],
+        [11, 8],
+      ],
       shipOrientation: "h",
     });
     expect(() => gameboard.placeShips("Tugboat")).toThrow();
@@ -57,7 +94,10 @@ describe("Gameboard", () => {
   test("placeShips method should accept valid coordinate pairs and orientation", () => {
     expect(gameboard.placeShips("destroyer", 1, 2, "H")).toStrictEqual({
       shipLength: 2,
-      shipCoordinates: [1, 2],
+      shipCoordinates: [
+        [1, 2],
+        [2, 2],
+      ],
       shipOrientation: "h",
     });
     expect(() => gameboard.placeShips("destroyer", -1, 2, "h")).toThrow();
@@ -65,7 +105,9 @@ describe("Gameboard", () => {
     expect(() => gameboard.placeShips("destroyer", 4, 5, "z")).toThrow();
   });
 
-  //   receiveAttack;
+  test("Gameboard has a receiveAttack method", () => {
+    expect(gameboard.receiveAttack(1, 1)).toBe("hit");
+  });
 
   //   missedAttacks;
 
