849e109f496f9fa6eab299f0d8bf9c3f4538ce89
mdot2x3 <mdot2x3@gmail.com>
2025-12-15 19:09:21 -0800

    feat: improve clicked cell eventListener logic
    turn the eventListener in event.js into a function
    called handleBoardClick.
    
    add check to handleBoardClick to verify that if a
    click causes a ship to sink it is logged to
    console, and afterwards run reportAllSunk() to
    check if the game is over and log to console if
    true along with deactivating the eventListener so
    additional clicks will no longer register on
    either gameboard.
    
    add check for class type in handleBoardClick to
    verify if a cell has already been clicked to
    prevent multiple clicks on the same cell, and if
    not, assign it the 'attacked' class when clicked.

--- src/events.js
+++ src/events.js
@@ -1,26 +1,41 @@
 export function setupEvents(player1, player2) {
-  document.addEventListener("DOMContentLoaded", (event) => {
+  document.addEventListener("DOMContentLoaded", () => {
     console.log("New Game code that runs on page load goes here.");
   });
 
-  // detect user clicks on gameboard, run receiveAttack() and mark board
   const domContent = document.querySelector(".dom-content");
-  domContent.addEventListener("click", (event) => {
+
+  function handleBoardClick(event) {
     if (!event.target.classList.contains("grid-cell")) return;
+
+    if (event.target.classList.contains("attacked")) return;
+    event.target.classList.add("attacked");
 
     const x = Number(event.target.dataset.x);
     const y = Number(event.target.dataset.y);
 
+    let attackResult, gameOver;
     if (event.target.parentElement.classList.contains("gameboard-left")) {
-      const attackResult = player1.gameboard.receiveAttack(x, y);
+      attackResult = player1.gameboard.receiveAttack(x, y);
       event.target.style.backgroundColor =
         attackResult === "miss" ? "blue" : "red";
+      if (attackResult === "sunk") console.log("Ship Sunk!");
+      gameOver = player1.gameboard.reportAllSunk();
     } else if (
       event.target.parentElement.classList.contains("gameboard-right")
     ) {
-      const attackResult = player2.gameboard.receiveAttack(x, y);
+      attackResult = player2.gameboard.receiveAttack(x, y);
       event.target.style.backgroundColor =
         attackResult === "miss" ? "blue" : "red";
+      if (attackResult === "sunk") console.log("Ship Sunk!");
+      gameOver = player2.gameboard.reportAllSunk();
     }
-  });
+
+    if (gameOver) {
+      console.log("Game Over");
+      domContent.removeEventListener("click", handleBoardClick);
+    }
+  }
+
+  domContent.addEventListener("click", handleBoardClick);
 }
